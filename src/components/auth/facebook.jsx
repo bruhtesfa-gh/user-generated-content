9605372f084e54f96cda0eec1baffc75008e1186
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2023-02-04 18:35:35 +0300

    scope removed

--- src/components/auth/facebook.jsx
+++ src/components/auth/facebook.jsx
@@ -4,11 +4,11 @@ import FacebookLogin from 'react-facebook-login';
 const FaceBook = () => {
     const [accessToken, setAccessToken] = useState('');
 
-    const componentClicked = () => {
-
+    const componentClicked = (data) => {
+        //console.log(data);
     }
-    const responseFacebook = () => {
-
+    const responseFacebook = (response) => {
+        console.log(response);
     }
     return <React.Fragment>
         <div style={{ marginLeft: "auto", marginRight: "auto" }}>
@@ -16,7 +16,6 @@ const FaceBook = () => {
                 appId="1252313545390634"
                 autoLoad={true}
                 fields="name,email,picture"
-                scope="public_profile,user_birthday,user_friends,user_likes,user_link,user_photos,user_posts,user_videos"
                 onClick={componentClicked}
                 callback={responseFacebook} />,
         </div>
